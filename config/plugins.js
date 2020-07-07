8302548aa1579c24de13fcd7a2053106495507fd
ramigs <ramigs@gmail.com>
2020-07-07 19:56:50 +0100

    Initial commit

---
+++ config/plugins.js
@@ -0,0 +1,6 @@
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    provider: "console",
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '975d6bdf-acba-46a9-9a81-6b1a5eb38c1c'
+};
